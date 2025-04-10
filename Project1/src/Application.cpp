ea7b480d4d906b7c95429db4ff0e9334caf0aae8
Yogesh <ypandey8859@gmail.com>
2025-04-10 17:42:44 +0530

    Add: error checking

--- Project1/src/Application.cpp
+++ Project1/src/Application.cpp
@@ -6,6 +6,24 @@
 #include <string>
 #include <sstream>
 
+#define ASSERT(x) if (!(x)) __debugbreak();
+#define GLLogCall(x) GLClearError();\
+    x;\
+    ASSERT(GLLogError(#x, __FILE__, __LINE__));
+
+static void GLClearError() {
+    while (glGetError() != GL_NO_ERROR);
+}
+
+static bool GLLogError(const char *function, const char *file, int line) {
+    if (GLenum errorCode = glGetError()) {
+        std::cout << "Error : " << errorCode << " At : " << function << " : " << line
+            << " In : " << file;
+        return false;
+    }
+    return true;
+}
+
 struct ShaderProgramSource
 {
     std::string VertexShader;
@@ -161,7 +179,7 @@ int main(void)
         glClear(GL_COLOR_BUFFER_BIT);
 
         //glDrawArrays(GL_TRIANGLES, 0, 6);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        GLLogCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
